d69c874015d654977471f1ecda2fefc7d180ba1c
Neha Maurya <pushpamaurya123456@gmail.com>
2026-06-05 11:22:31 +0530

    Added Day 9 programs

---
+++ Day_9/reverse_no_pattern.c
@@ -0,0 +1,20 @@
+// Write a program to print reverse number pattern //
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 5; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
