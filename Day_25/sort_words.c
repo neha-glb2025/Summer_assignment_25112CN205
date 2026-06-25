8f5fed8ef7464897a01bcd7806289231fc6cc646
Neha Maurya <nehamaurya100708@gmail.com>
2026-06-25 17:22:08 +0530

    Added Day_25 Programs

---
+++ Day_25/sort_words.c
@@ -0,0 +1,34 @@
+// Write a program to sort words by length //
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[10][50], temp[50];
+    int n, i, j;
+
+    printf("Enter number of words ");
+    scanf("%d", &n);
+
+    printf("Enter words");
+    for (i = 0; i < n; i++) {
+        scanf("%s", word[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (strlen(word[i]) > strlen(word[j])) {
+                strcpy(temp, word[i]);
+                strcpy(word[i], word[j]);
+                strcpy(word[j], temp);
+            }
+        }
+    }
+
+    printf("Words sorted by length");
+    for (i = 0; i < n; i++) {
+        printf("%s\n", word[i]);
+    }
+
+    return 0;
+}
